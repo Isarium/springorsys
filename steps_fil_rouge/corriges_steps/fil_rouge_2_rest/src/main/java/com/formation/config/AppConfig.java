fcdf38ce6f53d1443c6835e57240b653c945db51
naby <naby.gueye@gmail.com>
2023-09-06 16:42:16 +0200

    corrigé validation

--- steps_fil_rouge/corriges_steps/fil_rouge_2_rest/src/main/java/com/formation/config/AppConfig.java
+++ steps_fil_rouge/corriges_steps/fil_rouge_2_rest/src/main/java/com/formation/config/AppConfig.java
@@ -1,8 +1,13 @@
 package com.formation.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Configuration
 @ComponentScan("com.formation")
@@ -10,7 +15,20 @@ import org.springframework.context.annotation.PropertySource;
 // application.properties et les mettre à dispo
 @PropertySource("classpath:/application.properties")
 // Récupération dynamique du profil
-@PropertySource(value = "classpath:/application-${spring.profiles.active}.properties",ignoreResourceNotFound = true)
+@PropertySource(value = "classpath:/application-${spring.profiles.active}.properties", ignoreResourceNotFound = true)
+@EnableWebMvc // permet d'activer les beans pour spring mvc (convertisseurs ...)
 public class AppConfig {
+
+	// Définir un bean qui permet de faire la conversion objet<->json
+	// ce bean provient de la librairie jackson
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper();
+	}
+
+	@Bean
+	public MethodValidationPostProcessor methodValidationPostProcessor() {
+		return new MethodValidationPostProcessor();
+	}
 
 }
